55d4842d1bcf4f4d580dcd65942cbfbe181001c3
GonzaloCanelada <gonzalo.canelada@gmail.com>
2016-06-15 21:06:37 +0200

    Improved single image and added toggle in arguments

--- processor_pillow.py
+++ processor_pillow.py
@@ -4,29 +4,36 @@ from PIL import ImageColor
 from PIL import ImageDraw
 
 
+class ProcessType:
+    Single = 'single'
+    Dual = 'dual'
+    Two = 'two'
+
+
 class Processor:
     banner = None
     esif_logo = None
     resized_from = None
+    mode = None
 
-    def __init__(self, banner_path):
+    def __init__(self, banner_path, mode=ProcessType.Dual):
         self.banner = Image.open(banner_path)
         self.esif_logo = Image.open(os.path.join(os.path.dirname(__file__), "images/logo_esif.png"))
+        self.mode = mode
         print("banner", self.banner.format, self.banner.size, self.banner.mode)
 
     def process_image(self, photo_bundle):
-        items = len(photo_bundle.raw)
         parent_dir = os.path.dirname(photo_bundle.processed)
         if not os.path.exists(parent_dir):
             os.makedirs(parent_dir)
-        if items == 0:
-            print("ERROR")
-        elif items == 1:
-            self.process_single_image(photo_bundle).save(photo_bundle.processed)
-        elif items == 2:
-            self.process_two_images(photo_bundle).save(photo_bundle.processed)
+        if self.mode == ProcessType.Single:
+            return self.process_single_image(photo_bundle).save(photo_bundle.processed)
+        elif self.mode == ProcessType.Dual:
+            return self.process_dual_image(photo_bundle).save(photo_bundle.processed)
+        elif self.mode == ProcessType.Two:
+            return self.process_two_images(photo_bundle).save(photo_bundle.processed)
 
-    def dual_single_image(self, photo_bundle):
+    def process_dual_image(self, photo_bundle):
         im = Image.open(photo_bundle.raw[0])
         im.thumbnail(map(lambda x: int(x * 0.5), im.size), Image.ANTIALIAS)
         self.resize_additions(im)
@@ -39,7 +46,7 @@ class Processor:
         new_im = Image.new('RGBA', (new_width, new_height), ImageColor.getcolor('WHITE', 'RGBA'))
         draw = ImageDraw.Draw(new_im)
         line_y = top_border + im.size[1] + self.banner.size[1]
-        draw.line((0,line_y,new_width,line_y), fill=128, width=0)
+        draw.line((0, line_y, new_width, line_y), fill=128, width=0)
         banner_x_start = int(new_width / 2 - self.banner.size[0] / 2)
         new_im.paste(self.banner, (banner_x_start, top_border + im.size[1]))
         new_im.paste(self.banner, (banner_x_start, int(top_border * 1.5) + im.size[1] * 2 + self.banner.size[1]))
@@ -54,16 +61,22 @@ class Processor:
 
     def process_single_image(self, photo_bundle):
         im = Image.open(photo_bundle.raw[0])
+        im.thumbnail(map(lambda x: int(x * 0.5), im.size), Image.ANTIALIAS)
+        self.resize_additions(im)
         print("image", im.format, im.size, im.mode)
-
         new_height = int((im.size[1] + self.banner.size[1]) * 1.05)
         new_width = int(new_height * 1.5)
         top_border = int(new_height / 20)
         print("new", new_width, new_height, top_border)
 
         new_im = Image.new('RGBA', (new_width, new_height), ImageColor.getcolor('WHITE', 'RGBA'))
-        new_im.paste(self.banner, (int(new_width / 2 - self.banner.size[0] / 2), new_height - self.banner.size[1]))
-        new_im.paste(im, (int(new_width / 2 - im.size[0] / 2), top_border))
+        banner_x_start = int(new_width / 2 - self.banner.size[0] / 2)
+        new_im.paste(self.banner, (banner_x_start, top_border + im.size[1]))
+        im_x_start = int(new_width / 2 - im.size[0] / 2)
+        new_im.paste(im, (im_x_start, top_border))
+        esif_logo_x_start = int(new_width - im_x_start - self.esif_logo.size[0])
+        esif_logo_y_start = int(top_border + im.size[1])
+        new_im.paste(self.esif_logo, (esif_logo_x_start, esif_logo_y_start), mask=self.esif_logo)
         return new_im
 
     def process_two_images(self, photo_bundle):
