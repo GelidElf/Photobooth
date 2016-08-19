7fff4ef839c74455a60b60319464aac5a18ee0d0
GonzaloCanelada <gonzalo.canelada@gmail.com>
2016-08-20 00:03:47 +0200

    Added steps for taking 4 photos

--- window.py
+++ window.py
@@ -11,6 +11,27 @@ print_command = 'lpr -P ${printer} ${filename}'
 printers = ('Canon_CP910_ipp', 'Canon_CP910_ipp_b')
 
 
+def start_step():
+    if current_config.args.process == 'four':
+        return 'multiple1'
+    else:
+        return 'single'
+
+def number_of_photos():
+    if current_config.args.process == 'four':
+        return 4
+    else:
+        return 1
+
+
+def slide_name(number):
+    return 'Slide' + str(number) + '.JPG'
+
+
+def slide_names(start, number):
+    return [slide_name(x) for x in range(start, start + number)]
+
+
 class GameWindow:
     screen = None
     clock = None
@@ -30,15 +51,13 @@ class GameWindow:
         self.screen = screen
         self.clock = pygame.time.Clock()
         self.windows["welcome"] = Step(generator.welcome_path, [("menu", pygame.Rect((0, 0), self.screen.get_size()))])
-        self.windows["menu"] = Step('Slide2.JPG', [
-            ("single", pygame.Rect(self.screen.get_size()[0] * 0.25, 0, self.screen.get_size()[0] * 0.5,
-                                   self.screen.get_size()[1]), 1)])
-        self.windows["single"] = Step(['Slide3.JPG', 'Slide4.JPG', 'Slide5.JPG', 'Slide6.JPG', 'Slide7.JPG'], None,
-                                      ('smile', 1))
-        self.windows["single"] = Step(['Slide3.JPG', 'Slide4.JPG', 'Slide5.JPG', 'Slide6.JPG', 'Slide7.JPG'], None,
-                                      ('smile', 1))
-        self.windows["smile"] = Step('Slide8.JPG', None, command=('process', "PHOTO"))
+        self.windows["menu"] = Step('Slide2.JPG', [(start_step(), pygame.Rect(self.screen.get_size()[0] * 0.25, 0, self.screen.get_size()[0] * 0.5,self.screen.get_size()[1]), number_of_photos())])
+        self.wait_steps('single', 3, 'process')
         self.windows["process"] = Step('Slide9.JPG', None, ('single-result', 1))
+        self.wait_steps('multiple1', 10, 'multiple2')
+        self.wait_steps('multiple2', 16, 'multiple3')
+        self.wait_steps('multiple3', 22, 'multiple4')
+        self.wait_steps('multiple4', 28, 'process')
         return_to_menu = pygame.Rect((0, self.screen.get_size()[1] - 200), (200, self.screen.get_size()[1]))
         print_button = pygame.Rect((0, 0), (200, 200))
         self.windows["single-result"] = Step('Slide36.JPG', [('menu', return_to_menu), ('print', print_button)], ('welcome', 20), result=True)
@@ -48,6 +67,10 @@ class GameWindow:
         self.current_step = self.windows['welcome']
         self.screen_surface = pygame.Surface(self.screen.get_size())
         self.paint(self.current_step.screen(self.screen_surface, self.generator.last_photo_bundle, self.test_click_area))
+
+    def wait_steps(self, name, first_slide_number, next_step):
+        self.windows[name] = Step(slide_names(first_slide_number, 5), None, (name+'smile', 1))
+        self.windows[name+"smile"] = Step(slide_name(first_slide_number+5), None, command=(next_step, "PHOTO"))
 
     def transition(self, e):
         next_window_name = self.current_step.transition(e, self)
